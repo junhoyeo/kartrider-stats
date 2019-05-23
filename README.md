e13130a8a38b0c754a187c842d7bfe14b9530535
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-05-23 10:00:11 +0900

    fix: async function

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ```js
 > const kartriderStatus = require('./index.js')
-> kartriderStatus('nodim')
+> kartriderStatus('nodim').then((res) => { console.log(res) })
 [ { summary: '1전1승0패' },
   { summary: '5전1승4패' },
   { summary: '31전14승17패' } ]
--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 const cheerio = require('cheerio');
+const phantom = require('phantom');
 
-module.exports = function (nickname) {
+module.exports = async function (nickname) {
   QueryUrlBuilder = (query) => (`https://tmi.nexon.com/kart/user?nick=${encodeURI(query)}`);
   const url = QueryUrlBuilder(nickname);
-  const phantom = require('phantom');
 
-  (async function() {
-    const instance = await phantom.create();
-    const page = await instance.createPage();
+  const instance = await phantom.create();
+  const page = await instance.createPage();
 
-    const status = await page.open(url);
-    const $ = cheerio.load(await page.property('content'));
+  const status = await page.open(url);
+  const $ = cheerio.load(await page.property('content'));
 
-    let stat = []
-    $('div.stats').find('div.speed').each(function(idx, _) {
-      // console.log($.html(elem))
-      stat.push({
-        summary: $(this).find('.summary').text().trim()
-      })
-    });
-    console.log(stat)
+  let stat = []
+  $('div.stats').find('div.speed').each(function() {
+    // console.log($.html(elem))
+    stat.push({
+      summary: $(this).find('.summary').text().trim(),
 
-    await instance.exit();
-  })();
+    })
+  });
+
+  await instance.exit();
+  return stat;
 }
